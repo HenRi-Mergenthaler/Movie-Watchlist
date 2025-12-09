4316fd07d7fa7157caf11cd1dc19b3ba28f08233
HenRI <67016821+HenRi-Mergenthaler@users.noreply.github.com>
2025-12-09 08:59:38 -0300

    Mensagem de erro ao encontrar filme

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const mainEl = document.querySelector("main")
 const searchBtn = document.getElementById("search-btn")
 const searchInput = document.getElementById("search-input")
+const btnLoading = document.getElementById("btn-loading")
+const btnText = document.getElementById("btn-text")
 
 const apiKey = "1408e952"
 
@@ -9,16 +11,30 @@ searchBtn.addEventListener("click", e => {
 })
 
 async function searchFilm(searchStr) {
-    const res = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${searchStr}`)
-    const data = await res.json()
-
-    renderResults(data)
+    btnLoading.classList.toggle("disabled")
+    btnText.classList.toggle("disabled")
+    try {
+        const res = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${searchStr}`)
+        const data = await res.json()
+        
+        await renderResults(data)
+    } catch (err) {
+        console.error(err)
+    } finally {
+        btnLoading.classList.toggle("disabled")
+        btnText.classList.toggle("disabled")
+    }
+    
 }
 
 async function renderResults(data) {
+    //Se nao encontrado nenhum Filme, mostrar msg de erro
+    if (data.Response === "False") {
+        mainEl.innerHTML = `<div class="start-exploring-msg"><p>Unable to find what you’re looking for.<br> Please try another search.</p></div>`
+        return
+    }
+
     let htmlSnp = ""
-
-
     for (const film of data.Search) {
         const res = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${film.imdbID}`)
         const filmData = await res.json()
